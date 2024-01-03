11b6fb1eb3c3fa939707edf116f5f16086ff4d88
vemula muni varma <munimunnavemula123@gmail.com>
2024-01-03 23:19:20 +0530

    added signup and signin

--- controllers/users_Controller.js
+++ controllers/users_Controller.js
@@ -8,3 +8,14 @@ module.exports.usersPage = function(re,re){
     re.end("<h1>This is the users login psge for profile</h1>");
 }
 
+module.exports.sign_in = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'users | sign-in'
+    });
+}
+
+module.exports.sign_up = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'users | sign-up'
+    });
+}
--- routers/users.js
+++ routers/users.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controllers/users_Controller');
+const User = require('../models/UserSchema');
 
 router.get('/',UserController.usersPage);
 router.get('/profile', UserController.profile);
+router.get('/sign-in', UserController.sign_in);
+router.get('/sign-up', UserController.sign_up);
 
 module.exports = router;
